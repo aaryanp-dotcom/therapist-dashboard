4c58c6613b1e7362b747f6a1725ad9fdc43cb2ec
aaryanp-dotcom <aaryanparashar01@gmail.com>
2026-01-21 18:54:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -62,12 +62,11 @@ async function logout() {
 // DASHBOARD LOAD
 // ======================
 async function loadDashboard() {
-  const {
-    data: { session },
-  } = await supabase.auth.getSession();
+  const { data, error } = await supabase.auth.getSession();
 
-  if (!session) {
-    window.location.href = "login.html";
+  if (!data.session) {
+    localStorage.clear();
+    window.location.replace("login.html");
     return;
   }
 
